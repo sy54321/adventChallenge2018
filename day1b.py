619d6d8de5487debe0ea6c5b365c8dd95845c480
sy54321 <steve@Stephens-Mac-mini.local>
2018-12-08 14:54:25 -0600

    solved day1b

--- day1b.py
+++ day1b.py
@@ -2,10 +2,25 @@ import os
 dataFile = 'dataDay1.txt'
 currentFrequency = 0
 seenFrequencies = []
-with open(dataFile, 'r') as file:
-    for line in file:
-        if int(line) in seenFrequencies:
-            print(f'This frequency has been seen already - here is the answer: {int(line)}')
-            break
-        seenFrequencies.append(int(line))
-        currentFrequency += int(line)
+foundMatch = False
+def checkData():
+    global currentFrequency
+    global seenFrequencies
+    global foundMatch
+    if len(seenFrequencies)%5 == 0:
+        print(f'The program has checked {len(seenFrequencies)} frequencies.')
+    with open(dataFile, 'r') as file:
+        for line in file:
+            seenFrequencies.append(currentFrequency)
+            currentFrequency += int(line)
+            if currentFrequency in seenFrequencies:
+                print(f'This frequency has been seen already - here is the answer: {currentFrequency}')
+                print(f'The program checked {len(seenFrequencies)} frequencies.')
+                foundMatch = True
+                break
+            #print(seenFrequencies)
+
+while foundMatch == False:
+    checkData()
+
+print('Program Complete')
